38f9f55f92930e2b1eeed988ae23d67e72f65b81
KenAdBlock <kennycg13@gmail.com>
2016-12-12 15:54:42 +0100

    style(chat): Add begin of chat interface

---
+++ homiesApp/view/partials/chat.php
@@ -0,0 +1,18 @@
+<?php if(isset($_SESSION['user']) && !empty($_SESSION['user'])): ?>
+
+    <div id="chat-global">
+        
+        <div id="chat-header">Chat</div>
+
+        <div id="chat-body">
+            <div id="chat-content">Messages ici ...</div>
+    
+            <div id="chat-footer">
+                <textarea id="chat-footer-textarea" placeholder="Type your text here..."></textarea>
+                <span id="chat-footer-send"><i class="material-icons" title="Send your message">send</i></span>
+            </div>
+        </div>
+
+    </div>
+
+<?php endif; ?>
